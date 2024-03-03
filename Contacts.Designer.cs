fa4b88feaa8315f06aca9115532800382d47c8ea
Leo Yarovitsyn <152558488+taxoplasma@users.noreply.github.com>
2024-03-03 10:55:52 +0300

    Initial commit

---
+++ Contacts.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Lev_Contacts
+{
+    partial class Contacts
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(533, 369);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(16, 391);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(157, 391);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(132, 22);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(299, 389);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(407, 389);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(515, 389);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 28);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Изменить";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(16, 423);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(132, 22);
+            this.txtSearch.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(157, 421);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 28);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Найти";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 463);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "Contact Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ Contacts.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Npgsql;
+
+namespace Lev_Contacts
+{
+    public partial class Contacts : Form
+    {
+        private readonly string connectionString = "Host=localhost;Username=postgres;Password=8702;Database=Lev_Contacts";
+        private NpgsqlConnection connection;
+        private NpgsqlDataAdapter adapter;
+        private DataTable dataTable;
+
+        public Contacts()
+        {
+            InitializeComponent();
+            connection = new NpgsqlConnection(connectionString);
+            adapter = new NpgsqlDataAdapter("SELECT * FROM contacts", connection);
+            adapter.InsertCommand = new NpgsqlCommand("INSERT INTO contacts (name, phone) VALUES (@name, @phone)", connection);
+            adapter.InsertCommand.Parameters.Add("@name", NpgsqlTypes.NpgsqlDbType.Text, 255, "name");
+            adapter.InsertCommand.Parameters.Add("@phone", NpgsqlTypes.NpgsqlDbType.Text, 255, "phone");
+            adapter.UpdateCommand = new NpgsqlCommand("UPDATE contacts SET name = @name, phone = @phone WHERE id = @id", connection);
+            adapter.UpdateCommand.Parameters.Add("@name", NpgsqlTypes.NpgsqlDbType.Text, 255, "name");
+            adapter.UpdateCommand.Parameters.Add("@phone", NpgsqlTypes.NpgsqlDbType.Text, 255, "phone");
+            adapter.UpdateCommand.Parameters.Add("@id", NpgsqlTypes.NpgsqlDbType.Integer, 4, "id");
+            adapter.DeleteCommand = new NpgsqlCommand("DELETE FROM contacts WHERE id = @id", connection);
+            adapter.DeleteCommand.Parameters.Add("@id", NpgsqlTypes.NpgsqlDbType.Integer, 4, "id");
+            dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DataRow newRow = dataTable.NewRow();
+            newRow["name"] = txtName.Text;
+            newRow["phone"] = txtPhone.Text;
+            dataTable.Rows.Add(newRow);
+            adapter.Update(dataTable);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    int id = (int)row.Cells["id"].Value;
+                    string sql = "DELETE FROM contacts WHERE id = @id";
+                    using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                    {
+                        con.Open();
+                        using (NpgsqlCommand cmd = new NpgsqlCommand(sql, con))
+                        {
+                            cmd.Parameters.AddWithValue("@id", id);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    Console.WriteLine($"Удалён контакт с id {id}");
+                    dataTable.Rows.RemoveAt(row.Index);
+                }
+                adapter.Update(dataTable);
+            }
+            else
+            {
+                Console.WriteLine("Строки не выбраны");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                dataTable.Rows[rowIndex]["name"] = txtName.Text;
+                dataTable.Rows[rowIndex]["phone"] = txtPhone.Text;
+                adapter.Update(dataTable);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchName = txtSearch.Text;
+            DataRow[] foundRows = dataTable.Select($"name = '{searchName}'");
+            if (foundRows.Length > 0)
+            {
+                dataGridView1.DataSource = foundRows.CopyToDataTable();
+            }
+            else
+            {
+                MessageBox.Show("Контакт не найден.");
+            }
+        }
+    }
+}
